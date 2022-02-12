c6b78184e4fafc881b2b94d9032d57f0c729e9d0
ball9989 <62070006@it.kmitl.ac.th>
2022-02-12 21:14:27 +0700

    [UI] create sidebar navigation

--- frontend/component/NavTab.js
+++ frontend/component/NavTab.js
@@ -2,11 +2,54 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fa0, faBank, faChartLine, faChartPie, faFaceRelieved, faMoneyBill, faMoneyBillWave, faWallet } from '@fortawesome/free-solid-svg-icons'
 export default function NavTab() {
   return (
-    <div className='bg-black'>
-        <h1>asdasd</h1>
-    </div>
+    <SideNav className='bg-dark text-warning'
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="dashboard">
+        <NavItem eventKey="dashboard">
+            <NavIcon>
+                <FontAwesomeIcon icon={faChartPie} />
+            </NavIcon>
+            <NavText>
+                Dashboard
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="accounting">
+            <NavIcon>
+                <FontAwesomeIcon icon={faWallet} />
+            </NavIcon>
+            <NavText>
+                Accounting
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="installment">
+            <NavIcon>
+                <FontAwesomeIcon icon={faMoneyBillWave} />
+            </NavIcon>
+            <NavText>
+                Installment
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="interest">
+            <NavIcon>
+                <FontAwesomeIcon icon={faBank} />
+            </NavIcon>
+            <NavText>
+                Interest Rate
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
   )
 }
--- frontend/pages/accounting.js
+++ frontend/pages/accounting.js
@@ -1,19 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import NavTab from '../component/NavTab'
 
 export default function Accounting() {
   return (
-    <div>
-        <div className='row' style={{"height":"100vh"}}>
-            <div className='col-2 bg-black'>
-                <a>Dashboard</a>
-            </div>
-            <div className='col bg-warning'>
-
-            </div>
-        </div>
-    </div>
+    <NavTab></NavTab>
   )
 }
