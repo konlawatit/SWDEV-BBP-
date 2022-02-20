4d138dd11eece29de399df694c9a215f1b2a9287
bb0b <62070007@kmitl.ac.th>
2022-02-20 16:15:11 +0700

    [DESC] test ssl

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require("express")
-const forceSSL = require('express-force-ssl');
+// const forceSSL = require('express-force-ssl');
 var https = require('https');
 const cors = require("cors")
 const mongoose = require("mongoose")
 require('dotenv').config();
 
-let ssl_options = {
-    key: fs.readFileSync('./keys/private.key'),
-    cert: fs.readFileSync('./keys/cert.crt'),
-    ca: fs.readFileSync('./keys/intermediate.crt')
-  };
+// let ssl_options = {
+//     key: fs.readFileSync('./keys/private.key'),
+//     cert: fs.readFileSync('./keys/cert.crt'),
+//     ca: fs.readFileSync('./keys/intermediate.crt')
+//   };
+
+const privateKey = fs.readFileSync('server.key')
+const certificate = fs.readFileSync('server.cert')
 
 const PORT = 3030
 
@@ -21,7 +24,7 @@ const gmailAPI = require("./controllers/gmailAPI")
 const accounting = require("./controllers/accounting")
 const auth = require("./controllers/auth")
 
-var secureServer = https.createServer(ssl_options, app);
+// var secureServer = https.createServer(ssl_options, app);
 
 app.use(cors())
 
@@ -45,14 +48,14 @@ const routes = [
         target: accounting
     }
 ]
-app.use(forceSSL);
+// app.use(forceSSL);
 for (let route of routes) {
     app.use(route.prefix, route.target)
 }
 
 mongoose.connect(mongodbUri).then(result => {
-    secureServer.listen(443)
-    app.listen(PORT, () => {
+    // secureServer.listen(443)
+    https.createServer({key: privateKey, cert: certificate}, app).listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`)
     })
 }).catch(err => {
